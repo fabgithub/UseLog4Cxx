a948caeaac4cf5822fb0b8d4509133a17bc8fb85
fab <f@s.cn>
2017-11-08 15:14:10 +0800

    Initial Commit

---
+++ UseLog4Cxx/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  UseLog4Cxx
+//
+//  Created by fab on 2017/11/8.
+//  Copyright © 2017年 fab. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
